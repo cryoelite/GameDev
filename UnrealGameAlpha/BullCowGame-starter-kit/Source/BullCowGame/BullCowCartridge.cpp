d89dd574089cb95cb5b1758731f6f91b96ba79c9
Arvind Sagar <48724777+cryoelite@users.noreply.github.com>
2020-09-30 23:07:39 +0530

    logic-building

--- UnrealGameAlpha/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ UnrealGameAlpha/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,23 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    
+    HiddenWord=TEXT("CAKE");
+    int32 HwordLength{HiddenWord.Len()};
+
+    PrintLine(TEXT("Welcome to BullCow game yo"));
+    PrintLine(TEXT("Press Enter to get started."));
+    FString Opp{TEXT("Guess the ")};
+    Opp.AppendInt(HwordLength);
+    Opp.Append(TEXT(" letter word"));
+    PrintLine(Opp);
+
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
+    ClearScreen();
+    FString HiddenWord{TEXT("cow")};
 
+    PrintLine(Input);
 }
